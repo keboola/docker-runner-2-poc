405c765a2b233528d4517c383bbbb8fbff91cb4c
Martin Halamíček <martin@halamicek.cz>
2017-10-12 15:45:15 +0200

    aws batch control init

---
+++ cli/run.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinhalamicek
+ * Date: 12/10/2017
+ * Time: 15:38
+ */
+
+require_once 'vendor/autoload.php';
+
+
+$client = new \Aws\Batch\BatchClient([
+
+]);
