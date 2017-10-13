1bf7b9b6936e74ede8801ef22224f0735341c27f
Martin Halamíček <martin@halamicek.cz>
2017-10-13 14:29:29 +0200

    job termination and timeout

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM php:7.1-alpine
 MAINTAINER Martin Halamicek <Martin@keboola.com>
 
-RUN apk add --no-cache wget git unzip gzip docker
+RUN apk add --no-cache wget git unzip gzip docker libmcrypt-dev zlib-dev
+
+RUN docker-php-ext-install mcrypt zip pcntl
 
 COPY . /code/
 WORKDIR /code/
@@ -11,6 +13,5 @@ RUN ./composer.sh \
   && mv composer.phar /usr/local/bin/composer \
   && composer install --no-interaction \
   && apk del wget git unzip
-ADD . /code
 
 CMD php ./run.php
--- run.php
+++ run.php
@@ -1,23 +1,69 @@
 <?php
 require_once './vendor/autoload.php';
 
+
+function killContainer($containerId) {
+    $command = sprintf("docker kill %s", escapeshellarg($containerId));
+    $killProcess = new \Symfony\Component\Process\Process($command);
+    $killProcess->mustRun();
+}
+
 echo "START\n";
 
 $memory = getenv('KBC_CONTAINER_MEMORY') !== false ? getenv('KBC_CONTAINER_MEMORY') : '64m';
-$runTime = getenv('KBC_CONTAINER_RUN_TIME') !== false ? getenv('KBC_CONTAINER_RUN_TIME') : '60';
+$timeout = getenv('KBC_CONTAINER_TIMEOUT') !== false ?  (int) getenv('KBC_CONTAINER_TIMEOUT') : '240';
+$runTime = getenv('KBC_CONTAINER_RUN_TIME') !== false ? getenv('KBC_CONTAINER_RUN_TIME') : '120';
+
+$containerId = uniqid('kbc-run', true);
 
 $command = "docker run --rm"
     . " --memory " . escapeshellarg($memory)
     . " --memory-swap " . escapeshellarg($memory)
+    . " --name " . escapeshellarg($containerId)
     . " alpine sleep "
     . escapeshellarg($runTime)
     ;
 
 echo "Running command: {$command}\n";
+echo "Container timeout: {$timeout}\n";
 
 $process = new \Symfony\Component\Process\Process($command);
-$process
-    ->setTimeout(null)
-    ->mustRun();
+$process->setTimeout($timeout);
+
+// handle job termination
+pcntl_signal(SIGTERM, function ($signo) use($containerId)  {
+    // if we just kill the current process it will return 143 but the docker container remains running on host
+    // so we have to use docker kill
+    print "Job stopped by signal $signo";
+    killContainer($containerId);
+    print "Child container killed.";
+    exit(0);
+});
+
+$process->start(function ($type, $buffer) {
+    if (mb_strlen($buffer) > 64000) {
+        $buffer = mb_substr($buffer, 0, 64000) . " [trimmed]";
+    }
+    if ($type === \Symfony\Component\Process\Process::ERR) {
+        print 'ERR:' . $buffer . "\n";
+    } else {
+        print $buffer . "\n";
+    }
+});
+
+try {
+    do {
+        // check the signal
+        pcntl_signal_dispatch();
+        $process->checkTimeout();
+        sleep(1);
+    } while ($process->isRunning());
+} catch (\Symfony\Component\Process\Exception\ProcessTimedOutException $e) {
+    print "Container run timeout\n";
+    killContainer($containerId);
+    print "Child container killed.";
+}
+
+var_dump($process->getExitCode(), $process->getExitCodeText());
 
 echo "END\n";
